4f3bfdaf19b10c5247d95629b23bfb45f1366f84
chqng14 <phuongchang1402@gmail.com>
2023-09-12 14:31:39 +0700

    Add project files.

---
+++ App_View/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_View.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
