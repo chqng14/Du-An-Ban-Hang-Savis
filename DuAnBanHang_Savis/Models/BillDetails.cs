8a9ff8a401f2b1b3d7ef84ede1144feaff93bcf4
chqng14 <phuongchang1402@gmail.com>
2023-09-13 15:54:02 +0700

    AddModel BillDetails, Color, TypeProduct

---
+++ DuAnBanHang_Savis/Models/BillDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class BillDetails
+    {
+        public Guid Id { get; set; }
+        public Guid? IdBill { get; set; }
+        public Guid? IdProductDetail { get; set; }
+        public int? SoLuong { get; set; }
+        public decimal? DonGia { get; set; }
+        public int? TrangThai { get; set; }
+        //public virtual Bill Bill { get; set; }
+        //public virtual ProductDetail ProductDetail { get; set; }
+    }
+}
---
+++ DuAnBanHang_Savis/Models/DBContextModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace App_Data.Models
+{
+    public class DBContextModel : DbContext
+    {
+        public DBContextModel()
+        {
+
+        }
+
+        public DBContextModel(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        //public DbSet<Bill> Bills { get; set; }
+        //public DbSet<BillDetail> BillDetails { get; set; }
+        //public DbSet<Cart> Carts { get; set; }
+        //public DbSet<CartDetail> CartDetails { get; set; }
+        //public DbSet<Color> Colors { get; set; }
+        //public DbSet<SaleDetail> DetailSales { get; set; }
+        //public DbSet<Image> Images { get; set; }
+        //public DbSet<Material> Materials { get; set; }
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<ProductDetail> ProductDetails { get; set; }
+        //public DbSet<Role> Roles { get; set; }
+        //public DbSet<Sale> Sales { get; set; }
+        //public DbSet<Size> Sizes { get; set; }
+        //public DbSet<TypeProduct> TypeProducts { get; set; }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Voucher> Vouchers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=MSI;Initial Catalog=DuAnBanHang_Savis;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.
+                ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
