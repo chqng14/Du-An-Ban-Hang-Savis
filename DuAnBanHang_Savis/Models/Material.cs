7df7d8421a1a5dc2c42a22ab1d8b2c726a5e6a1c
DESKTOP-GEBT4IE\hoang <boxchatsc1@gmail.com>
2023-09-13 16:31:09 +0700

    add models: Marterial,ProductDetail and Suppliers

---
+++ DuAnBanHang_Savis/Models/Material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class Material
+    {
+        public Guid Id { get; set; }
+        [MaxLength(250)]
+        public string Ten { get; set; }
+        public int TrangThai { get; set; }
+    }
+}
