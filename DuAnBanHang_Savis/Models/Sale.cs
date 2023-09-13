937e4966531bece982d5bae4c11ea32657efa713
DESKTOP-8RSAFN0\anhtr <anhtrinhtran3@gmail.com>
2023-09-13 15:45:38 +0700

    add model Voucher,Sale,SaleDetail,Blog

---
+++ DuAnBanHang_Savis/Models/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App_Data.Models
+{
+    public partial class Sale
+    {
+        public Guid Id { get; set; }
+        public string? Ma { get; set; }
+        public string? Ten { get; set; }
+        public DateTime? NgayBatDau { get; set; }
+        public DateTime? NgayKetThuc { get; set; }
+        public string? LoaiHinhKm { get; set; }
+        public decimal? MucGiam { get; set; }
+        public string? MoTa { get; set; }
+        public int? TrangThai { get; set; }
+        public virtual ICollection<SaleDetail> DetailSales { get; set; }
+    }
+}
---
+++ DuAnBanHang_Savis/Models/SaleDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App_Data.Models
+{
+    public partial class SaleDetail
+    {
+        public Guid Id { get; set; }
+        public Guid? IdSale { get; set; }
+        public Guid? IdChiTietSp { get; set; }
+        public string? MoTa { get; set; }
+        public int? TrangThai { get; set; }
+        //public virtual ProductDetail? ProductDetail { get; set; }
+        public virtual Sale? Sale { get; set; }  
+        
+
+    }
+}
