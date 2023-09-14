4eae9ed24de70c8936ca31f8ea95ec18fe75e677
DESKTOP-GEBT4IE\hoang <boxchatsc1@gmail.com>
2023-09-14 19:53:58 +0700

    add code productDetailController

--- App_Api/Controllers/ProductDetailController.cs
+++ App_Api/Controllers/ProductDetailController.cs
@@ -14,17 +14,18 @@ namespace App_Api.Controllers
         private readonly IAllRepo<Color> _allRepoColor;
         private readonly IAllRepo<Product> _allRepoProduct;
         private readonly IAllRepo<Size> _allRepoSize;
-        public ProductDetailController(IAllRepo<ProductDetails> allRepoProductDetail, IAllRepo<Material> allRepoMaterial, IAllRepo<TypeProduct> allRepoTypeProduct, IAllRepo<Color> allRepoColor, IAllRepo<Product> allRepoProduct)
+        public ProductDetailController(IAllRepo<ProductDetails> allRepoProductDetail, IAllRepo<Material> allRepoMaterial, IAllRepo<TypeProduct> allRepoTypeProduct, IAllRepo<Color> allRepoColor, IAllRepo<Product> allRepoProduct, IAllRepo<Size> allRepoSize)
         {
             _allRepoProductDetail = allRepoProductDetail;
             _allRepoMaterial = allRepoMaterial;
             _allRepoTypeProduct = allRepoTypeProduct;
             _allRepoColor = allRepoColor;
             _allRepoProduct = allRepoProduct;
+            _allRepoSize = allRepoSize;
         }
 
         [HttpPost("create-productdetail")]
-        public IActionResult CreateProductDetail([FromBody] ProductDetailDTO productDetailDTO)
+        public IActionResult CreateProductDetail([FromBody]ProductDetailDTO productDetailDTO)
         {
             productDetailDTO.TrangThai = 1;
             var productDetail = new MapperConfiguration(cfg =>
@@ -41,14 +42,14 @@ namespace App_Api.Controllers
             {
                 Id = item.Id,
                 BaoHanh = item.BaoHanh,
-                ChatLieu = _allRepoMaterial.GetAll().FirstOrDefault(x=>x.Id==item.IdMaterial)?.Ten,
+                ChatLieu = _allRepoMaterial.GetAll().FirstOrDefault(x => x.Id == item.IdMaterial)?.Ten,
                 GiaBan = item.GiaBan,
                 GiaNhap = item.GiaNhap,
-                Loai = _allRepoTypeProduct.GetAll().FirstOrDefault(lo=>lo.Id == item.IdTypeProduct)?.Ten,
-                MauSac = _allRepoColor.GetAll().FirstOrDefault(co=>co.Id == item.IdColor)?.Ten,
+                Loai = _allRepoTypeProduct.GetAll().FirstOrDefault(lo => lo.Id == item.IdTypeProduct)?.Ten,
+                MauSac = _allRepoColor.GetAll().FirstOrDefault(co => co.Id == item.IdColor)?.Ten,
                 MoTa = item.MoTa,
-                NameProduct = _allRepoProduct.GetAll().FirstOrDefault(na=>na.Id == item.IdProduct)?.Ten,
-                Size = _allRepoSize.GetAll().FirstOrDefault(si=>si.Id == item.IdSize)?.Ten,
+                NameProduct = _allRepoProduct.GetAll().FirstOrDefault(na => na.Id == item.IdProduct)?.Ten,
+                Size = _allRepoSize.GetAll().FirstOrDefault(si => si.Id == item.IdSize)?.Ten,
                 SoLuongTon = item.SoLuongTon,
                 TrangThai = item.TrangThai,
 
@@ -56,6 +57,41 @@ namespace App_Api.Controllers
             return new OkObjectResult(listProductDetailViewModel);
         }
 
+        [HttpDelete("delete")]
+        public bool DeleteProductDetail(List<Guid> lstGuid)
+        {
+            try
+            {
+                foreach (var id in lstGuid)
+                {
+                    _allRepoProductDetail.RemoveItem(_allRepoProductDetail.GetAll().FirstOrDefault(pro => pro.Id == id));
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
 
+        [HttpPut("update")]
+        public bool Update([FromBody]ProductDetailDTO productDetailDTO)
+        {
+            try
+            {
+                var productDetail = new MapperConfiguration(cfg =>
+                cfg.CreateMap<ProductDetailDTO, ProductDetails>()
+            ).CreateMapper().Map<ProductDetails>(productDetailDTO);
+                _allRepoProductDetail.EditItem(productDetail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+
+        }
     }
 }
