f26ed5ae7bded07be24e1b4279835304248db847
DESKTOP-GEBT4IE\hoang <boxchatsc1@gmail.com>
2023-09-14 17:55:33 +0700

    add code productDetailController

--- App_Api/Controllers/ProductDetailController.cs
+++ App_Api/Controllers/ProductDetailController.cs
@@ -1,18 +1,61 @@
 ﻿using App_Data.IRepositories;
 using App_Data.Models;
+using App_Data.ViewModels.ProductDetail;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App_Api.Controllers
 {
     public class ProductDetailController
     {
-        private readonly IAllRepo<ProductDetails> _allRepoProduct;
-
-        public ProductDetailController(IAllRepo<ProductDetails> allRepoProduct)
+        private readonly IAllRepo<ProductDetails> _allRepoProductDetail;
+        private readonly IAllRepo<Material> _allRepoMaterial;
+        private readonly IAllRepo<TypeProduct> _allRepoTypeProduct;
+        private readonly IAllRepo<Color> _allRepoColor;
+        private readonly IAllRepo<Product> _allRepoProduct;
+        private readonly IAllRepo<Size> _allRepoSize;
+        public ProductDetailController(IAllRepo<ProductDetails> allRepoProductDetail, IAllRepo<Material> allRepoMaterial, IAllRepo<TypeProduct> allRepoTypeProduct, IAllRepo<Color> allRepoColor, IAllRepo<Product> allRepoProduct)
         {
+            _allRepoProductDetail = allRepoProductDetail;
+            _allRepoMaterial = allRepoMaterial;
+            _allRepoTypeProduct = allRepoTypeProduct;
+            _allRepoColor = allRepoColor;
             _allRepoProduct = allRepoProduct;
         }
 
-        
+        [HttpPost("create-productdetail")]
+        public IActionResult CreateProductDetail([FromBody] ProductDetailDTO productDetailDTO)
+        {
+            productDetailDTO.TrangThai = 1;
+            var productDetail = new MapperConfiguration(cfg =>
+                cfg.CreateMap<ProductDetailDTO, ProductDetails>()
+            ).CreateMapper().Map<ProductDetails>(productDetailDTO);
+
+            return new OkObjectResult(new { success = _allRepoProductDetail.AddItem(productDetail), id = productDetail.Id });
+        }
+
+        [HttpGet("get-list-productdetail")]
+        public IActionResult GetListProductDetail()
+        {
+            var listProductDetailViewModel = _allRepoProductDetail.GetAll().Select(item => new ProductViewModel
+            {
+                Id = item.Id,
+                BaoHanh = item.BaoHanh,
+                ChatLieu = _allRepoMaterial.GetAll().FirstOrDefault(x=>x.Id==item.IdMaterial)?.Ten,
+                GiaBan = item.GiaBan,
+                GiaNhap = item.GiaNhap,
+                Loai = _allRepoTypeProduct.GetAll().FirstOrDefault(lo=>lo.Id == item.IdTypeProduct)?.Ten,
+                MauSac = _allRepoColor.GetAll().FirstOrDefault(co=>co.Id == item.IdColor)?.Ten,
+                MoTa = item.MoTa,
+                NameProduct = _allRepoProduct.GetAll().FirstOrDefault(na=>na.Id == item.IdProduct)?.Ten,
+                Size = _allRepoSize.GetAll().FirstOrDefault(si=>si.Id == item.IdSize)?.Ten,
+                SoLuongTon = item.SoLuongTon,
+                TrangThai = item.TrangThai,
+
+            }).ToList();
+            return new OkObjectResult(listProductDetailViewModel);
+        }
+
+
     }
 }
---
+++ DuAnBanHang_Savis/ViewModels/ProductDetail/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.ViewModels.ProductDetail
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+        public string? NameProduct { get; set; }
+        public string? MauSac { get; set; }
+        public String? Size { get; set; }
+        public string? Loai { get; set; }
+        public string? ChatLieu { get; set; }
+        public string? BaoHanh { get; set; }
+        public string? MoTa { get; set; }
+        public int SoLuongTon { get; set; }
+        public decimal? GiaNhap { get; set; }
+        public decimal? GiaBan { get; set; }
+        public int? TrangThai { get; set; }
+
+    }
+}
