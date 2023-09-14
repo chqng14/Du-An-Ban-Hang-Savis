0773669c27b7ad9a76d6501033c4339ad332128c
chqng14 <phuongchang1402@gmail.com>
2023-09-14 15:18:59 +0700

    Them Repositories

---
+++ DuAnBanHang_Savis/IRepositories/IAllRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.IRepositories
+{
+    public interface IAllRepo<T>
+    {
+        public IEnumerable<T> GetAll();
+        public bool AddItem(T item);
+        public bool RemoveItem(T item);
+        public bool EditItem(T item);
+        public string MaTS();
+    }
+}
