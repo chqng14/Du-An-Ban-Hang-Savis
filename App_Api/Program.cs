0b865171c6b66ebda0fcd373e9173dd637f00e34
vietdung71103@gmail.com <vietdung71103@gmail.com>
2023-09-14 16:01:03 +0700

    add role controller

--- App_Api/Program.cs
+++ App_Api/Program.cs
@@ -1,3 +1,7 @@
+using App_Data.IRepositories;
+using App_Data.Models;
+using App_Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
