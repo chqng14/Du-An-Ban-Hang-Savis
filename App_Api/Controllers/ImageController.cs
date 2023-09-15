64fbd77f85e16248d213e120db8afc192e91170f
DESKTOP-GEBT4IE\hoang <boxchatsc1@gmail.com>
2023-09-15 15:32:07 +0700

    add image controller

---
+++ App_Api/Controllers/ImageController.cs
@@ -0,0 +1,70 @@
+﻿using App_Data.IRepositories;
+using App_Data.Models;
+using App_Data.ViewModel;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly IAllRepo<Images> _allRepoImage;
+
+        public ImageController(IAllRepo<Images> allRepoImage)
+        {
+            _allRepoImage = allRepoImage;
+        }
+
+        public async Task<IActionResult> CreateImage([FromForm] Guid idProductDetail, [FromForm] List<IFormFile> lstIFormFile)
+        {
+            //string currentDirectory = Directory.GetCurrentDirectory();
+            //string rootPath = Directory.GetParent(currentDirectory).FullName;
+            //string uploadDirectory = Path.Combine(rootPath, "App_Api", "wwwroot", "Images");
+
+            //foreach (var file in lstIFormFile)
+            //{
+            //    if (file.Length > 0)
+            //    {
+            //        using (var stream = new MemoryStream())
+            //        {
+            //            file.CopyTo(stream);
+            //            stream.Position = 0;
+
+            //            using (var image = SixLabors.ImageSharp.Image.Load(stream))
+            //            {
+            //                if (image.Width > 400 || image.Height > 300)
+            //                {
+            //                    image.Mutate(x => x.Resize(new ResizeOptions
+            //                    {
+            //                        Size = new SixLabors.ImageSharp.Size(400, 300),
+            //                        Mode = ResizeMode.Max
+            //                    }));
+            //                }
+
+            //                var encoder = new JpegEncoder
+            //                {
+            //                    Quality = 80
+            //                };
+
+            //                string fileName = Guid.NewGuid().ToString() + file.FileName;
+            //                string outputPath = Path.Combine(uploadDirectory, fileName);
+
+            //                using (var outputStream = new FileStream(outputPath, FileMode.Create))
+            //                {
+            //                    image.Save(outputStream, encoder);
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            return Ok();
+        }
+
+
+    
+    }
+}
--- App_Api/Controllers/ProductDetailController.cs
+++ App_Api/Controllers/ProductDetailController.cs
@@ -1,4 +1,5 @@
-﻿using App_Data.IRepositories;
+﻿using App_Data.Configurations;
+using App_Data.IRepositories;
 using App_Data.Models;
 using App_Data.ViewModels.ProductDetail;
 using AutoMapper;
@@ -25,7 +26,7 @@ namespace App_Api.Controllers
         }
 
         [HttpPost("create-productdetail")]
-        public IActionResult CreateProductDetail([FromBody]ProductDetailDTO productDetailDTO)
+        public IActionResult CreateProductDetail([FromBody] ProductDetailDTO productDetailDTO)
         {
             productDetailDTO.TrangThai = 1;
             var productDetail = new MapperConfiguration(cfg =>
@@ -33,6 +34,12 @@ namespace App_Api.Controllers
             ).CreateMapper().Map<ProductDetails>(productDetailDTO);
 
             return new OkObjectResult(new { success = _allRepoProductDetail.AddItem(productDetail), id = productDetail.Id });
+        }
+
+        [HttpGet("get-productdetail")]
+        public ProductDetails? GetProductDetail(Guid idProductDetail)
+        {
+            return _allRepoProductDetail.GetAll().FirstOrDefault(pro => pro.Id == idProductDetail);
         }
 
         [HttpGet("get-list-productdetail")]
@@ -48,8 +55,8 @@ namespace App_Api.Controllers
                 Loai = _allRepoTypeProduct.GetAll().FirstOrDefault(lo => lo.Id == item.IdTypeProduct)?.Ten,
                 MauSac = _allRepoColor.GetAll().FirstOrDefault(co => co.Id == item.IdColor)?.Ten,
                 MoTa = item.MoTa,
-                NameProduct = _allRepoProduct.GetAll().FirstOrDefault(na=>na.Id == item.IdProduct)?.Ten,
-                Size = _allRepoSize.GetAll().FirstOrDefault(si=>si.Id == item.IdSize)?.Ten,
+                NameProduct = _allRepoProduct.GetAll().FirstOrDefault(na => na.Id == item.IdProduct)?.Ten,
+                Size = _allRepoSize.GetAll().FirstOrDefault(si => si.Id == item.IdSize)?.Size1,
                 SoLuongTon = item.SoLuongTon,
                 TrangThai = item.TrangThai,
 
@@ -76,10 +83,14 @@ namespace App_Api.Controllers
         }
 
         [HttpPut("update")]
-        public bool Update([FromBody]ProductDetailDTO productDetailDTO)
+        public bool Update([FromBody] ProductDetailDTO productDetailDTO)
         {
             try
             {
+                if (GetProductDetail(productDetailDTO.Id) == null)
+                {
+                    return false;
+                }
                 var productDetail = new MapperConfiguration(cfg =>
                 cfg.CreateMap<ProductDetailDTO, ProductDetails>()
             ).CreateMapper().Map<ProductDetails>(productDetailDTO);
